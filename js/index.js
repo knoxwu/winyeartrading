4e66b1603a1325d19e17fa6c65aee451e175c9cb
Han <gaohan.henry@Gmail.com>
2016-11-21 16:47:21 -0800

    init website

---
+++ js/index.js
@@ -0,0 +1,67 @@
+
+(function(){
+
+  var app = angular.module('items',[]);
+
+  app.controller('ItemsController',function(){
+      this.item = pro;
+
+
+  });
+
+
+  var pro = [
+    {
+      num: 0,
+      name: 'Birds on Trees',
+      rate: 20,
+      photo: 'book-1.JPG',
+    },
+    {
+      num: 1,
+      name: 'Ears of Wheat',
+      rate: 20,
+      photo: 'book-2.JPG',
+    },
+    {
+      num: 2,
+      name: 'Flower',
+      rate: 20,
+      photo: 'book-3.JPG',
+    },
+    {
+      num: 3,
+      name: 'Flower',
+      rate: 20,
+      photo: 'book-3.JPG',
+    },
+    {
+      num: 4,
+      name: 'Birds on Trees',
+      rate: 20,
+      photo: 'book-1.JPG',
+    },
+    {
+      num: 5,
+      name: 'Ears of Wheat',
+      rate: 20,
+      photo: 'book-2.JPG',
+    },
+    {
+      num: 6,
+      name: 'Flower',
+      rate: 20,
+      photo: 'book-3.JPG',
+    },
+    {
+      num: 7,
+      name: 'Flower',
+      rate: 20,
+      photo: 'book-3.JPG',
+    },
+    
+  ];
+
+
+
+})();
